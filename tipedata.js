6655ddab74885f3933900faf1650c017a4f93755
irfanarga <irfanargaprasetya81@gmail.com>
2021-12-23 21:38:24 +0700

    checkpoint tugas4

---
+++ tipedata.js
@@ -0,0 +1,14 @@
+function tipedata() {
+    var data = '2';
+
+    console.log(typeof(data))
+
+    if(data === 2){
+        return 'sukses'
+    }else{
+        return 'gagal'
+    }
+
+}
+
+console.log(tipedata());
---
+++ tugas4.js
@@ -0,0 +1,17 @@
+var siswa1 = 150;
+var siswa2 = 200;
+var siswa3 = 190;
+
+if(siswa1>siswa2) {
+    if(siswa1>siswa3) {
+        console.log("siswa1 paling tinggi");
+    }else{
+        console.log("siswa1 tertinggi kedua");
+    }
+}else{
+    if(siswa2>siswa3) {
+        console.log("siswa2>siswa3>siswa1");
+    }else{
+        console.log("siswa2 tertinggi kedua");
+    }
+}
