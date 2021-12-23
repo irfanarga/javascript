ec59bdb9789b02497fed07f03d781b7350fe3e15
irfanarga <irfanargaprasetya81@gmail.com>
2021-12-23 20:29:01 +0700

    checkpoint tugas3

---
+++ tugas3.js
@@ -0,0 +1,11 @@
+function game() {
+    var genre = ["sport", "casual", "adventure"];
+    return genre;
+}
+var type = game();
+console.log(type);
+
+function calc(x,y,z) {
+    console.log(x+y+z);
+}
+calc(7,8,9);
